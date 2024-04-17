736befffd775fd2a2a091d0ed8feb827f1ef3ee6
Eitan Turok <eitan.turok@databricks.com>
2024-04-17 15:55:39 +0000

    add list of possible functions to system prompt

--- src/tooltalk/evaluation/evaluate_openai.py
+++ src/tooltalk/evaluation/evaluate_openai.py
@@ -23,11 +23,18 @@ from transformers import AutoTokenizer
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+SYSTEM_PROMPT = """"You are an expert in composing functions. You are given a question and a set of possible functions. Based on the question, you will need to make one or more function/tool calls to achieve the purpose. If none of the function can be used, point it out. If the given question lacks the parameters required by the function, also point it out.
+Here is a list of functions in JSON format that you can invoke:\n{functions}.
+Should you decide to return the function call(s), write "FUNCTION_CALL" and put each function call in the format of [func1(params_name=params_value, params_name2=params_value2...), func2(params)]
+Do NOT include any other text in your response! You MUST write FUNCTION_CALL before responding with any the function calls and put all function calls in a list.
+
+Here is some user data:
+location: {location}
+timestamp: {timestamp}
+username (if logged in): {username}
+"""
+
 class OpenAIPredictor(BaseAPIPredictor):
-    system_prompt = "You are a helpful assistant. Here is some user data:" \
-                    "\nlocation: {location}" \
-                    "\ntimestamp: {timestamp}" \
-                    "\nusername (if logged in): {username}"
 
     def __init__(self, client, model, apis_used, disable_docs=False):
         self.client=client
@@ -40,7 +47,9 @@ class OpenAIPredictor(BaseAPIPredictor):
             )
 
     def predict(self, metadata: dict, conversation_history: dict) -> dict:
-        system_prompt = self.system_prompt.format(
+        api_docs_str = '\n'.join([json.dumps(api_doc) for api_doc in self.api_docs])
+        system_prompt = SYSTEM_PROMPT.format(
+            functions=api_docs_str,
             location=metadata["location"],
             timestamp=metadata["timestamp"],
             username=metadata.get("username")
