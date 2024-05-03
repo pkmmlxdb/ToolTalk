25fb167db4194a1344b6d418953f7832a4261325
Eitan Turok <eitan.turok@databricks.com>
2024-05-03 13:57:34 +0000

    add parsing of output

--- src/tooltalk/evaluation/predictors/dbrx_predictor.py
+++ src/tooltalk/evaluation/predictors/dbrx_predictor.py
@@ -4,7 +4,7 @@ import logging
 from tooltalk.evaluation.tool_executor import BaseAPIPredictor
 from transformers import AutoTokenizer
 
-from .constants import SYSTEM_PROMPT
+from .constants import DBRX_SYSTEM_PROMPT
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -23,9 +23,28 @@ class DBRXPredictor(BaseAPIPredictor):
             token="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",
             )
 
+    def get_function_call(self, message):
+        func = message.split("FUNCTION_CALL")[-1]
+        func = func.strip(" \n")
+        if not func.startswith("["):
+            func = "[" + func
+        if not func.endswith("]"):
+            func = func + "]"
+        if func.startswith("['"):
+            func = func.replace("['", "[")
+        if func.endswith("]'"):
+            func = func.replace("]'", "]")
+        return func
+
+    def parse_function_call(self, function_call):
+        name = function_call.split("(")[0][1:]
+        args = function_call.split("(")[1][:-2]
+        return name, args
+
+
     def predict(self, metadata: dict, conversation_history: dict) -> dict:
         api_docs_str = '\n'.join([json.dumps(api_doc) for api_doc in self.api_docs])
-        system_prompt = SYSTEM_PROMPT.format(
+        system_prompt = DBRX_SYSTEM_PROMPT.format(
             functions=api_docs_str,
             location=metadata["location"],
             timestamp=metadata["timestamp"],
@@ -68,7 +87,6 @@ class DBRXPredictor(BaseAPIPredictor):
             )
         logger.debug(f"OpenAI full response: {openai_response}")
         openai_message = openai_response.choices[0].text
-
         # metadata = {
         #     "openai_request": {
         #         "model": self.model,
@@ -77,16 +95,16 @@ class DBRXPredictor(BaseAPIPredictor):
         #     },
         #     "openai_response": "" #openai_response
         # }
-        # metadata = {
         metadata = {}
-        if "function_call" in openai_message:
-            function_call = openai_message["function_call"]
-            api_name = function_call["name"]
+
+        if "FUNCTION_CALL" in openai_message:
+            function_call = self.get_function_call(openai_message)
+            api_name, parameters_str = self.parse_function_call(function_call)
             try:
-                parameters = json.loads(function_call["arguments"])
+                parameters = json.loads(parameters_str)
             except json.decoder.JSONDecodeError:
                 # check termination reason
-                logger.info(f"Failed to decode arguments for {api_name}: {function_call['arguments']}")
+                logger.info(f"Failed to decode arguments for {api_name}: {parameters_str}")
                 parameters = None
             return {
                 "role": "api",
@@ -94,13 +112,11 @@ class DBRXPredictor(BaseAPIPredictor):
                     "api_name": api_name,
                     "parameters": parameters
                 },
-                # store metadata about call
                 "metadata": metadata,
             }
         else:
             return {
                 "role": "assistant",
                 "text": openai_message,
-                # store metadata about call
                 "metadata": metadata,
             }
